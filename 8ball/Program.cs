54d9bc054bd30b7d6af45d5d8f2b782fe5365d1d
WonderI3iscuit <58702197+WonderI3iscuit@users.noreply.github.com>
2020-10-06 09:05:10 -0400

    Add files via upload

--- 8ball/Program.cs
+++ 8ball/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Threading;
+using System.Linq;
+
 
 namespace _8ball
 {
@@ -23,33 +26,74 @@ namespace _8ball
 
         public static void QuestionLoop()
         {
-            
+                
                 Random rand = new Random();
+
+            Console.ForegroundColor = (ConsoleColor)rand.Next(15);
                 int randomNumber = rand.Next(4);
                 switch (randomNumber)
                 {
                     case 0:
                         {
-                            Console.WriteLine("Yes!");
+                        Console.ForegroundColor = (ConsoleColor)rand.Next(15);
+                        Console.WriteLine("Yes!");
                             break;
                         }
                     case 1:
                         {
-                            Console.WriteLine("No!");
+                        Console.ForegroundColor = (ConsoleColor)rand.Next(15);
+                        Console.WriteLine("No!");
                             break;
                         }
                     case 2:
                         {
-                            Console.WriteLine("Maybe so...");
+                        Console.ForegroundColor = (ConsoleColor)rand.Next(15);
+                        Console.WriteLine("Maybe so...");
                             break;
                         }
                     case 3:
                         {
-                            Console.WriteLine("Try again later!");
+                        Console.ForegroundColor = (ConsoleColor)rand.Next(15);
+                        Console.WriteLine("Try again later!");
+                            break;
+                        }
+                    case 4:
+                        {
+                        Console.ForegroundColor = (ConsoleColor)rand.Next(15);
+                        Console.WriteLine("Why are you bothering me??");
+                            break;
+                        }
+                    case 5:
+                        {
+                        Console.ForegroundColor = (ConsoleColor)rand.Next(15);
+                        Console.WriteLine("what? do you want me to solve all of your problems?!");
+                            break;
+                        }
+                    case 6:
+                        {
+                        Console.ForegroundColor = (ConsoleColor)rand.Next(15);
+                        Console.WriteLine("In your dreams!");
+                            break;
+                        }
+                    case 7:
+                        {
+                        Console.ForegroundColor = (ConsoleColor)rand.Next(15);
+                        Console.WriteLine("100%");
+                            break;
+                        }
+                    case 8:
+                        {
+                        Console.ForegroundColor = (ConsoleColor)rand.Next(15);
+                        Console.WriteLine("eh... maybe...?");
+                            break;
+                        }
+                    case 9:
+                        {
+                        Console.ForegroundColor = (ConsoleColor)rand.Next(15);
+                        Console.WriteLine("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
                             break;
                         }
 
-                
             }
 
         }
